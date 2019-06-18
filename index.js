3f93c773570976d85d2be94e20a670b2d4b7ba61
Joy Lin <vvlnote@hotmail.com>
2019-06-18 22:47:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+class Driver {
+  constructor (name, strJoinDate) {
+    this.name = name;
+    this.joinDate = new Date(strJoinDate);
+  }
+  startDate() {
+    return this.strJoinDate;
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.joinDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  getEastWestBlocks(startingStreet, endingStreet) {
+    const eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+    let blocks = eastWest.indexOf(endingStreet) - eastWest.indexOf(startingStreet);
+    return blocks > 0 ? blocks : (blocks * -1);
+  }
+
+  getNorthSouthBlocks(startingStreet, endingStreet) {
+    let blocks = Number(endingStreet) - Number(startingStreet);
+    return blocks >0 ? blocks : (blocks * -1);
+  }
+  
+  blocksTravelled() {
+    let horizontalBlocks = getEastWestBlocks(this.beginningLocation.horizontal, this.endingLocation.horizontal);
+    let verticalBlocks = getNorthSouthBlocks(this.beginningLocation.vertical, this.endingLocation.vertical);
+    return (horizontalBlocks + verticalBlocks);
+  }
+
+  estimatedTime() {
+
+  }
+}
