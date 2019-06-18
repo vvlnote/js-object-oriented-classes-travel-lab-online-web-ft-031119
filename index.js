75cea7df2e5d2a9b21c62823af7723a1c2f13628
Joy Lin <vvlnote@hotmail.com>
2019-06-18 22:07:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+class Driver {
+  constructor (name, strJoinDate) {
+    this.name = name;
+    this.joinDate = new Date(strJoinDate);
+  }
+  startDate() {
+    return this.strJoinDate;
+  }
 
+  yearsExperienceFromBeginningOf(year) {
+    return year - this.joinDate.getFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+
+  }
+
+  estimatedTime() {
+
+  }
+}
